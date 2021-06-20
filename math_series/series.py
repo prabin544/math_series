28efb7dfb1935aed2860375db81034988d607f19
Prabin Singh <pravin544@gmail.com>
2021-06-20 10:39:05 -0500

    added match series solution

--- math_series/series.py
+++ math_series/series.py
@@ -1,46 +1,30 @@
 def fibonacci(n):
-    """
-    Return nth value in the fibonacci series 
-
-    """
-    n1, n2 = 0, 1
-    count = 0
-    flst = []
-    while count < n:
-        flst.append(n1)
-        nth = n1 + n2     
-        n1 = n2           
-        n2 = nth          
-        count += 1
-    return flst[n-1]
+  """
+  Return the nth value in the fibonacci series.
+  """
+  if n <= 1:
+    return 0
+  elif n == 2:
+    return 1
+  else:
+    return fibonacci(n-1) + fibonacci(n-2)
 
 def lucas(n):
-    """
-    Return nth value in the lucas numbers 
-    
-    """
-    n1, n2 = 2, 1
-    count = 0
-    llst = []
-    while count < n:
-        llst.append(n1)
-        nth = n1 + n2     
-        n1 = n2           
-        n2 = nth          
-        count += 1
-    return llst[n-2]
+  """
+  Returns the nth value in the lucas numbers
+  """
+  if n <= 0:
+      return 2
+  if n == 1: 
+      return 1
+  return lucas(n-1) + lucas(n-2)
 
-# def sum_series(n, x=0, y=1):
-#     if n == 0:
-#         return x
-#     if n == 1:
-#         return y
-#     slst = [] * n + 1
-#     slst[0] = x
-#     slst[1] = y
-
-#     for items in slst:
-#         slst[items] = slst[items - 1] + slst
-#         return slst[n]
-
-    
+def sum_series(n, x=0, y=1):
+  """
+  Calling this function with no optional parameters will produce numbers from the fibonacci series. Calling it with the optional arguments 2 and 1 will produce values from the lucas numbers.
+  """
+  if n == 0:
+    return x
+  if n == 1:
+    return y
+  return sum_series(n-1, x, y) + sum_series(n-2, x, y)
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,21 +1,23 @@
-from math_series.series import fibonacci, lucas
+from math_series.series import fibonacci, lucas, sum_series
 
-# fibonacci series
-# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55.....
+# Fibonacci Tests
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+
 def test_one():
   actual = fibonacci(11)
   expected = 55
   assert actual == expected
 
 def test_two():
-  actual = fibonacci(9)
-  expected = 21
+  actual = fibonacci(6)
+  expected = 5
   assert actual == expected
 
-# Lucas Numbers
-# 2, 1, 3, 4, 7, 11, 18, 29, 47
+# Lucas Tests
+# 2, 1, 3, 4, 7, 11, 18, 29, 47, 76, 123
+
 def test_three():
-  actual = lucas(10)
+  actual = lucas(8)
   expected = 47
   assert actual == expected
 
@@ -24,14 +26,14 @@ def test_four():
   expected = 7
   assert actual == expected
 
-# # sum_series
+# Sum Series Tests
 
-# def test_five():
-#   actual = lucas(0)
-#   expected = 0
-#   assert actual == expected
+def test_five():
+  actual = sum_series(0)
+  expected = 0
+  assert actual == expected
 
-# def test_six():
-#   actual = lucas(2, 2, 1)
-#   expected = 3
-#   assert actual == expected
+def test_six():
+  actual = sum_series(2, 2, 1)
+  expected = 3
+  assert actual == expected
